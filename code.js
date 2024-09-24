542f10ecf9dbb199dc02585247af772d13e517f1
22dcolli <144738226+22dcolli@users.noreply.github.com>
2024-09-24 10:45:38 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
 function fib(n) {
-    return n;
+    let arr = [0,1]
+
+    if (n==0){
+        return[0];
+    }
+    else if(n==1){
+        return arr;
+    }
+    else{
+        for(let i=2;i<=n;i++){
+              arr.push(arr[i-1]+arr[i-2])
+            }
+    }
+    return arr;
 }
